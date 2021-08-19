762caf3530e05352578037c4c88b77e43bddf9a0
Crowdbotics <team@crowdbotics.com>
2021-08-19 13:27:26 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "waste_no_goods_29945Identifier",
+  url: "https://waste_no_goods_29945Identifier.botics.co",
+  api: "https://waste_no_goods_29945Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
